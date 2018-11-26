d2a7a2000877bd99c7ef710c0fa54594b7793dab
Nichole Monhait <nicholemonhait@Nicholes-MacBook-Pro.local>
2018-11-26 15:55:36 -0700

    relocate map

--- writing/map_il.R
+++ writing/map_il.R
@@ -6,16 +6,18 @@ library(sf)
 library(leaflet)
 #bring in polgyon info for IL counties
 #IL FIPS = 17
-il_counties <- counties(state = "IL", cb = TRUE, class = "sf")
+il_counties <- counties(state = "IL", cb = TRUE, class = "sf") %>% 
+  st_set_crs(NA) %>% st_set_crs(4326)
 
 il_skeleton <- il_counties %>% 
   ggplot() +
   geom_sf(data = il_counties) 
 
+
 #leaflet package
 
 il <- leaflet(il_counties) %>%
+  setView(lng = -89.3985, lat = 40.6331, zoom = 8) %>% 
   addProviderTiles("OpenStreetMap.BlackAndWhite") %>%  
   addPolygons(color = "black", weight = 1, fillOpacity = 0)
-
 
