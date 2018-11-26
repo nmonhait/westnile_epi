8bae18e66d4c9574ec66983f59f3e11b0ad77d45
Nichole Monhait <nicholemonhait@Nicholes-MacBook-Pro.local>
2018-11-26 15:50:01 -0700

    IL map change crs #

--- map_il.R
+++ map_il.R
@@ -6,11 +6,13 @@ library(sf)
 library(leaflet)
 #bring in polgyon info for IL counties
 #IL FIPS = 17
-il_counties <- counties(state = "IL", cb = TRUE, class = "sf")
+il_counties <- counties(state = "IL", cb = TRUE, class = "sf") %>% 
+  st_set_crs(NA) %>% st_set_crs(4326)
 
 il_skeleton <- il_counties %>% 
   ggplot() +
   geom_sf(data = il_counties) 
+
 
 #leaflet package
 
@@ -18,3 +20,4 @@ il <- leaflet(il_counties) %>%
   setView(lng = -89.3985, lat = 40.6331, zoom = 8) %>% 
   addProviderTiles("OpenStreetMap.BlackAndWhite") %>%  
   addPolygons(color = "black", weight = 1, fillOpacity = 0)
+
