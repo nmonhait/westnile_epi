7e469567275fffd4b644d373d385e64231c64b8b
Kathleen Wendt <Kathleen Wendt>
2018-11-28 13:20:07 -0700

    clean, delete, and move files

--- writing/map_il.R
+++ writing/map_il.R
@@ -4,10 +4,14 @@ library(ggplot2)
 library(ggthemes)
 library(sf)
 library(leaflet)
+library(sp)
 #bring in polgyon info for IL counties
 #IL FIPS = 17
 il_counties <- counties(state = "IL", cb = TRUE, class = "sf") %>% 
-  st_set_crs(NA) %>% st_set_crs(4326)
+  st_set_crs(NA) %>% 
+  st_set_crs(4326)
+
+il_sp <- sf:::as_Spatial(il_counties)
 
 il_skeleton <- il_counties %>% 
   ggplot() +
@@ -16,10 +20,11 @@ il_skeleton <- il_counties %>%
 
 #leaflet package
 
-il <- leaflet(il_counties) %>%
-  setView(lng = -89.3985, lat = 40.6331, zoom = 8) %>% 
-  addProviderTiles("OpenStreetMap.BlackAndWhite") %>%  
-  addPolygons(color = "black", weight = 1, fillOpacity = 0)
+il <- leaflet(il_sp) %>%
+  addPolygons() %>% 
+  # setView(lng = -89.3985, lat = 40.6331, zoom = 8) %>% 
+  addProviderTiles("OpenStreetMap.BlackAndWhite") # %>%  
+  # addPolygons(color = "black", weight = 1, fillOpacity = 0)
 
 il
 
