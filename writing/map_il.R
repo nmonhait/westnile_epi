4076793eee1ad22637c0699c99fa01c728ccb35e
Nichole Monhait <nicholemonhait@Nicholes-MacBook-Pro.local>
2018-11-28 12:02:09 -0700

    joined data

--- writing/map_il.R
+++ writing/map_il.R
@@ -9,6 +9,8 @@ library(leaflet)
 il_counties <- counties(state = "IL", cb = TRUE, class = "sf") %>% 
   st_set_crs(NA) %>% st_set_crs(4326)
 
+il_sp <- sf:::as_Spatial(il_counties)
+
 il_skeleton <- il_counties %>% 
   ggplot() +
   geom_sf(data = il_counties) 
@@ -16,7 +18,20 @@ il_skeleton <- il_counties %>%
 
 #leaflet package
 
-il <- leaflet(il_counties) %>%
-  setView(lng = -89.3985, lat = 40.6331, zoom = 8) %>% 
-  addProviderTiles("OpenStreetMap.BlackAndWhite") %>%  
-  addPolygons(color = "black", weight = 1, fillOpacity = 0)
+il <- leaflet() %>%
+  addPolygons(data = il_sp) %>% 
+  addProviderTiles("OpenStreetMap.BlackAndWhite") 
+
+
+il
+
+#add latitude and longitude to original dataset
+
+library(readr)
+data_il <- read_csv("data/data_il.csv") %>% 
+  rename(NAME = il)
+
+il_counties_2 <- il_counties %>% 
+  select(STATEFP, COUNTYFP, geometry, NAME) 
+
+full_il <- inner_join(data_il, il_counties, by = "NAME")
