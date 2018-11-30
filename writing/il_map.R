2476788642aea1411a608f09dc9beddd010a06f8
Kathleen Wendt <Kathleen Wendt>
2018-11-30 13:23:55 -0700

    switch from server.R and ui.R to only app.R

--- writing/il_map.R
+++ writing/il_map.R
@@ -1,4 +1,4 @@
-#load required libraries 
+# load required libraries 
 library(tidyverse)
 library(tigris)
 library(ggplot2)
@@ -10,19 +10,19 @@ library(readr)
 library(viridis)
 library(dplyr)
 
-#West nile df
+# West nile df
 il_wnv <-read_csv("data/data_il.csv") %>% 
   subset(il != "IL") %>% #some values do not have county level data and are indicated as IL
   rename(NAME = il) %>% 
   group_by(NAME) %>% 
   count()
 
-#Tigris IL df
+# Tigris IL df
 il_counties <- counties(state = "IL", cb = TRUE, class = "sf") %>% 
   st_set_crs(NA) %>%
   st_set_crs(4326)
 
-#fuzzy merge il_wnv and il_counties
+# merge il_wnv and il_counties
 full_il <- full_join(il_wnv, il_counties, by = "NAME") %>% 
   group_by(NAME) %>% 
   ungroup() %>% 
@@ -36,7 +36,7 @@ case_count_full <- full_il %>%
   group_by(NAME) %>% 
   count()
 
-#interactive leaflet plot
+# interactive leaflet plot
 bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
 pal <- leaflet::colorFactor((viridis_pal(option = "inferno",
                                          begin = 1, end = 0.2)(4)), 
@@ -44,7 +44,7 @@ pal <- leaflet::colorFactor((viridis_pal(option = "inferno",
 
 labels <- sprintf(
   "<strong>%s</strong><br/> <sup></sup>",
-  full_il$NAME #case_count_full$n
+  full_il$NAME # case_count_full$n
 ) %>% lapply(htmltools::HTML)
 
 il_cases <- leaflet(full_il) %>%
