87c8055cd90bc6edc4541981ffd3546d22b56c0b
Kathleen Wendt <Kathleen Wendt>
2018-12-05 12:04:17 -0700

    split

--- il_app/server.R
+++ il_app/server.R
@@ -18,25 +18,25 @@ library(RColorBrewer)
 # source("il_app/helper.R")
 
 
-ui <- bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(top = 10, right = 10,
-                sliderInput("range", "Magnitudes", 
-                            min(quakes$mag), 
-                            max(quakes$mag),
-                            value = range(quakes$mag), 
-                            step = 0.1
-                ),
-                selectInput("colors", "Color Scheme",
-                            rownames(subset(brewer.pal.info, 
-                                            category %in% c("seq", "div")))
-                ),
-                checkboxInput("legend", 
-                              "Show legend", 
-                              TRUE)
-  )
-)
+# ui <- bootstrapPage(
+#   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+#   leafletOutput("map", width = "100%", height = "100%"),
+#   absolutePanel(top = 10, right = 10,
+#                 sliderInput("range", "Magnitudes", 
+#                             min(quakes$mag), 
+#                             max(quakes$mag),
+#                             value = range(quakes$mag), 
+#                             step = 0.1
+#                 ),
+#                 selectInput("colors", "Color Scheme",
+#                             rownames(subset(brewer.pal.info, 
+#                                             category %in% c("seq", "div")))
+#                 ),
+#                 checkboxInput("legend", 
+#                               "Show legend", 
+#                               TRUE)
+#   )
+# )
 
 server <- function(input, output, session) {
   
