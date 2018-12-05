e279a5bdd1fdf8c948589c44cd764a48994a0fb5
Kathleen Wendt <Kathleen Wendt>
2018-12-04 19:31:27 -0700

    dashboard update

--- il_app/ui.R
+++ il_app/ui.R
@@ -10,21 +10,19 @@ library(DT)
 library(ggplot2)
 library(ggmap)
 
-# source map file
-
-# source("writing/il_map.R")
 
 shinyUI(dashboardPage(
-  
   dashboardHeader(title = "Demographic patterns of West Nile Virus in Illinois,
                   2005 - 2012",
                   titleWidth = 1000),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
     fluidRow(
-      column(width = 7,
+      column(width = 7, 
              box(width = NULL,
-<<<<<<< HEAD
+                 leafletOutput("map", height = 500))),
+      column(width = 10,
+             box(width = 10,
                  sliderInput(inputId = "year",
                              label = "Select the year(s): ",
                              value = c(2005, 2012), # not in date form
@@ -35,8 +33,8 @@ shinyUI(dashboardPage(
                              #animationOptions(interval = 1) - use if in date form?
                              ))
                  ),
-      column(width = 5,
-             box(width = NULL,
+      column(width = 7,
+             box(width = 7,
                  selectizeInput(inputId = "gender", # gender widget
                              label = "Select gender(s): ",
                              choices = c("Male", 
@@ -46,44 +44,17 @@ shinyUI(dashboardPage(
              ),
                  selectizeInput(inputId = "race", # race widget
                              label = "Select race(s): ",
-=======
-                 leafletOutput("map", height = 400))),
-      column(width = 5,
-             box(width = NULL,
-                 checkboxGroupInput(inputId = "gender",
-                             label = "Filter based on patient gender: ",
-                             choices = c("Male", 
-                                       "Female"),
-                             selected =  c("Male", 
-                                           "Female"))
-             ),
-                 checkboxGroupInput(inputId = "race",
-                             label = "Filter based on patient race: ",
->>>>>>> 8f4244f4801fe15d6a4ff1059ce37ce5e2c5b7dd
                              choices = c("White", 
-                                       "BlackAfricanAmerican", # same as data labels?
-                                       "Asian",
-                                       "Multiple Races",
-                                       "Other",
-                                       "Unknown",
-                                       "na"),
-<<<<<<< HEAD
-                             multiple = TRUE) 
+                                         "BlackAfricanAmerican", 
+                                         "Asian",
+                                         "Multiple Races",
+                                         "Other",
+                                         "Unknown",
+                                         "na"),
+                             multiple = TRUE)
              ),
                 selectizeInput(inputId = "age", # age widget
                             label = "Select age group(s): ",
-=======
-                             selected = c("White", 
-                                          "BlackAfricanAmerican", # same as data labels?
-                                          "Asian",
-                                          "Multiple Races",
-                                          "Other",
-                                          "Unknown",
-                                          "na"))
-             ),
-                checkboxGroupInput(inputId = "age",
-                            label = "Filter based on patient age group: ",
->>>>>>> 8f4244f4801fe15d6a4ff1059ce37ce5e2c5b7dd
                             choices = c("0-4 Years",
                                         "5-9 Years",
                                         "10-14 Years",
@@ -97,29 +68,8 @@ shinyUI(dashboardPage(
                                         "50-54 Years",
                                         "55-59 Years",
                                         "60-64 Years",
-                                        "65 + Years",
-                                        "na"),
-<<<<<<< HEAD
-                            multiple = TRUE)
-=======
-                            selected = c("0-4 Years",
-                                         "5-9 Years",
-                                         "10-14 Years",
-                                         "15-19 Years",
-                                         "20-24 Years",
-                                         "25-29 Years",
-                                         "30-34 Years",
-                                         "35-39 Years",
-                                         "40-44 Years",
-                                         "45-49 Years",
-                                         "50-54 Years",
-                                         "55-59 Years",
-                                         "60-64 Years",
-                                         "65 + Years",
-                                         "na"))
->>>>>>> 8f4244f4801fe15d6a4ff1059ce37ce5e2c5b7dd
-      )
+                                        "65 + Years"),
+                            multiple = TRUE))
     ),
-  skin = "blue"))
-
+  skin = "purple"))
   
