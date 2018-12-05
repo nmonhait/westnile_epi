888f880c6b01d7eed766f128707a6d5ad75d7fd9
Kathleen Wendt <Kathleen Wendt>
2018-12-04 18:14:44 -0700

    multiple inputs in widgets

--- il_app/ui.R
+++ il_app/ui.R
@@ -36,20 +36,19 @@ shinyUI(dashboardPage( # create shiny dashboard fluid page
                              max = 2012,
                              ticks = TRUE,
                              animate = TRUE 
-                             #animationOptions(interval = 1)
+                             #animationOptions(interval = 1) - use if in date form?
                              ))
                  ),
       column(width = 5,
              box(width = NULL,
-                 checkboxGroupInput(inputId = "gender", # gender widget
+                 selectizeInput(inputId = "gender", # gender widget
                              label = "Select gender(s): ",
                              choices = c("Male", 
                                        "Female",
                                        "na"),
-                             selected =  c("Male", 
-                                           "Female")) 
+                             multiple = TRUE) 
              ),
-                 checkboxGroupInput(inputId = "race", # race widget
+                 selectizeInput(inputId = "race", # race widget
                              label = "Select race(s): ",
                              choices = c("White", 
                                        "BlackAfricanAmerican", # same as data labels? 
@@ -58,13 +57,9 @@ shinyUI(dashboardPage( # create shiny dashboard fluid page
                                        "Other",
                                        "Unknown",
                                        "na"),
-                             selected = c("White", 
-                                          "BlackAfricanAmerican", # same as data labels?
-                                          "Asian",
-                                          "Multiple Races",
-                                          "Other")) 
+                             multiple = TRUE) 
              ),
-                checkboxGroupInput(inputId = "age", # age widget
+                selectizeInput(inputId = "age", # age widget
                             label = "Select age group(s): ",
                             choices = c("0-4 Years",
                                         "5-9 Years",
@@ -81,20 +76,7 @@ shinyUI(dashboardPage( # create shiny dashboard fluid page
                                         "60-64 Years",
                                         "65 + Years",
                                         "na"),
-                            selected = c("0-4 Years",
-                                         "5-9 Years",
-                                         "10-14 Years",
-                                         "15-19 Years",
-                                         "20-24 Years",
-                                         "25-29 Years",
-                                         "30-34 Years",
-                                         "35-39 Years",
-                                         "40-44 Years",
-                                         "45-49 Years",
-                                         "50-54 Years",
-                                         "55-59 Years",
-                                         "60-64 Years",
-                                         "65 + Years"))
+                            multiple = TRUE)
       )
     ),
   skin = "blue"))
