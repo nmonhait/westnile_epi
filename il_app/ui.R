83ffc7e590ccbfa04f88239a3def30b081e8cce8
Randy Xun <randyxun@gmail.com>
2018-12-05 17:03:03 -0700

    New UI

--- il_app/ui.R
+++ il_app/ui.R
@@ -24,82 +24,6 @@ shinyUI(dashboardPage(
   dashboardBody(
     fluidRow(
       column(width = 7, # map widget
-             box(width = NULL,
-                 leafletOutput("map", height = 400))), 
-      column(width = 5, # year slider widget
-             box(width = NULL,
-                 sliderInput(inputId = "year",
-                             label = "Select the year(s): ",
-                             value = c(2005, 2012), # not in date form
-                             min = 2005,
-                             max = 2012,
-                             ticks = TRUE,
-                             animate = TRUE 
-                             #animationOptions(interval = 1)
-                 ))
-      ),
-      column(width = 5,
-             box(width = NULL,
-                 checkboxGroupInput(inputId = "gender", # gender widget
-                                    label = "Select gender(s): ",
-                                    choices = c("Male", 
-                                                "Female",
-                                                "na"),
-                                    selected =  c("Male", 
-                                                  "Female")) 
-             ),
-             checkboxGroupInput(inputId = "race", # race widget
-                                label = "Select race(s): ",
-                                choices = c("White", 
-                                            "BlackAfricanAmerican", # same as data labels? 
-                                            "Asian",
-                                            "Multiple Races",
-                                            "Other",
-                                            "Unknown",
-                                            "na"),
-                                selected = c("White", 
-                                             "BlackAfricanAmerican", # same as data labels?
-                                             "Asian",
-                                             "Multiple Races",
-                                             "Other")) 
-      ),
-      checkboxGroupInput(inputId = "age", # age widget
-                         label = "Select age group(s): ",
-                         choices = c("0-4 Years",
-                                     "5-9 Years",
-                                     "10-14 Years",
-                                     "15-19 Years",
-                                     "20-24 Years",
-                                     "25-29 Years",
-                                     "30-34 Years",
-                                     "35-39 Years",
-                                     "40-44 Years",
-                                     "45-49 Years",
-                                     "50-54 Years",
-                                     "55-59 Years",
-                                     "60-64 Years",
-                                     "65 + Years",
-                                     "na"),
-                         selected = c("0-4 Years",
-                                      "5-9 Years",
-                                      "10-14 Years",
-                                      "15-19 Years",
-                                      "20-24 Years",
-                                      "25-29 Years",
-                                      "30-34 Years",
-                                      "35-39 Years",
-                                      "40-44 Years",
-                                      "45-49 Years",
-                                      "50-54 Years",
-                                      "55-59 Years",
-                                      "60-64 Years",
-                                      "65 + Years"))
-    )
-  ),
-  skin = "blue"))
-
-
-column(7, 
        box(width = NULL, offset = 0,
            leafletOutput("map", height = 500))),
 column(width = 4, offset = 0,
@@ -147,6 +71,6 @@ column(width = 11,
                        ticks = TRUE
            ))
 ),
-skin = "navy")))
+skin = "purple")))
 
   
