b928c14ca32244bda733e07b810f9b32d3420a1c
Kathleen Wendt <Kathleen Wendt>
2018-12-07 11:42:49 -0700

    delete unnecessary code

--- Illinois/app.R
+++ Illinois/app.R
@@ -19,7 +19,7 @@ ui <- dashboardPage(
                   titleWidth = 1000),
   # disable sidebar
   dashboardSidebar(disable = TRUE),
-  # set dashboard body with map, "one option widget", and year slider
+  # set dashboard body with map, gender widget, and year slider
   dashboardBody(
     fluidRow(
       column(width = 12, # map widget
@@ -36,7 +36,7 @@ ui <- dashboardPage(
                            sep = "", # to remove commas from numeric years
                            ticks = TRUE
                )),
-           box(width = 3, offset = 9, # sex widget
+           box(width = 3, offset = 9, # gender widget
               radioButtons("gender", label = "Choose gender",
                             choiceNames = list(
                                 HTML("Male"), 
@@ -74,15 +74,6 @@ server <- function (input, output, session) {
     sliderValues()
   })
   
-  # year_filter <- reactive({
-  #   il_demog[il_demog$year >= input$]
-  # })
-  # 
-  # # Reactive expression for the data subsetted to what the user selected
-  #   filteredData <- reactive({
-  #     quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
-  #   })
-  # 
   # create leaflet map
   output$map <- renderLeaflet({
     pal <- leaflet::colorNumeric("Blues", # change color; quantiles/breaks
@@ -113,102 +104,5 @@ server <- function (input, output, session) {
   })
 }
 
-
-
 shinyApp(ui = ui, server = server)
-
-
-
-###################### example ################
-
-
-# ui <- bootstrapPage(
-#   tags$style(type = "text/css",
-#              "html,
-#              body {width:100%;height:100%}"),
-#   leafletOutput("map",
-#                 width = "100%",
-#                 height = "100%"),
-#   absolutePanel(top = 10,
-#                 right = 10,
-#                 sliderInput("range",
-#                             "Magnitudes",
-#                             min(quakes$mag),
-#                             max(quakes$mag),
-#                             value = range(quakes$mag),
-#                             step = 0.1
-#                 ),
-#                 selectInput("colors",
-#                             "Color Scheme",
-#                             rownames(subset(brewer.pal.info,
-#                                             category %in% c("seq", "div")))
-#                 ),
-#                 checkboxInput("legend",
-#                               "Show legend",
-#                               TRUE)
-#   )
-# )
-
-
-
-# server <- function(input, output, session) {
-# 
-#   # Reactive expression for the data subsetted to what the user selected
-  # filteredData <- reactive({
-  #   quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
-  # })
-# 
-#   # This reactive expression represents the palette function,
-#   # which changes as the user makes selections in UI.
-#   colorpal <- reactive({
-#     colorNumeric(input$colors, quakes$mag)
-#   })
-# 
-#   output$map <- renderLeaflet({
-#     # Use leaflet() here, and only include aspects of the map that
-#     # won't need to change dynamically (at least, not unless the
-#     # entire map is being torn down and recreated).
-#     leaflet(quakes) %>% addTiles() %>%
-#       fitBounds(~min(long),
-#                 ~min(lat),
-#                 ~max(long),
-#                 ~max(lat))
-#   })
-# 
-#   # Incremental changes to the map (in this case, replacing the
-#   # circles when a new color is chosen) should be performed in
-#   # an observer. Each independent set of things that can change
-#   # should be managed in its own observer.
-#   observe({
-#     pal <- colorpal()
-# 
-#     leafletProxy("map", data = filteredData()) %>%
-#       clearShapes() %>%
-#       addCircles(radius = ~10^mag/10,
-#                  weight = 1,
-#                  color = "#777777",
-#                  fillColor = ~pal(mag),
-#                  fillOpacity = 0.7,
-#                  popup = ~paste(mag)
-#       )
-#   })
-# 
-#   # Use a separate observer to recreate the legend as needed.
-#   observe({
-#     proxy <- leafletProxy("map", data = quakes)
-# 
-#     # Remove any existing legend, and only if the legend is
-#     # enabled, create a new one.
-#     proxy %>% clearControls()
-#     if (input$legend) {
-#       pal <- colorpal()
-#       proxy %>% addLegend(position = "bottomright",
-#                           pal = pal,
-#                           values = ~mag
-#       )
-#     }
-#   })
-# }
-# 
-# shinyApp(ui, server)
 
